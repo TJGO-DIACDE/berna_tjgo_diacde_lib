7b1dd86e3f06c4ba177318c2c2de8223cd005cd6
TJGO-DIACDE <diacde@tjgo.jus.br>
2024-07-26 15:52:50 -0300

    Update preProcessamento.py

--- preProcessamento.py
+++ preProcessamento.py
@@ -1,6 +1,10 @@
+import pandas as pd
+import pkg_resources
+
 def clear(txt: str) -> str:
     out_txt = txt.lower().replace('style@page','').replace('style','').replace('70px','').replace('\"span ','').replace('p&ampnbsp','').replace(' p ','').replace('\"lineheight"','').replace('textindent','').replace('justify','').replace('150%','').replace(' /p ','').replace('100px','').replace('\"fontsize','').replace('marginleft','').replace('70px','').replace('&ampnbsp','').replace('\"fontfamily','').replace('80px','').replace('30px','').replace('100%','').replace('marginright','').replace(' margin ','').replace('\"textalign','').replace('\"fontfamily','').replace('','').replace(' times ','').replace(' br ','').replace(' span ','').replace('lineheight','').replace('fontsize','').replace('fontfamily','').replace('textalign','').replace(' p ','').replace('2016p','').replace('3cm','').replace('4cm','').replace('p&ampnbsp','').replace(' new ','').replace('romanspan','').replace('&ampnbsp','')
-    out_txt = ''.join([w for w in out_txt if w.isalnum() or w==' '])
+    out_txt = ''.join([l for l in out_txt if l.isalnum() or l==' '])
+    out_txt = ' '.join([w for w in out_txt.split() if len(w) > 2])
 
     return out_txt
 
@@ -40,6 +44,27 @@ def get_synonym(txt: str) -> str:
 
     return txt
 
+def get_synonym_by_dict(txt: str) -> str:
+    table_dict = _read_binary()
+    txt = txt.lower()
+
+    for _, row in table_dict.iterrows():
+
+        for token in str(row['DE_PARA']).split(','):
+            token = token.lower()
+
+            if token in txt:
+                txt = txt.replace(token, row['PALAVRA'].strip(', '))
+
+    return txt
+            
+
+def _read_binary() -> object:
+    file_local = pkg_resources.resource_filename(__name__, 'data/data.pkl')
+    df_carregado = pd.read_pickle(file_local)
+
+    return df_carregado
+
 
 if __name__=='__main__':
     # Teste func 1
@@ -47,3 +72,6 @@ if __name__=='__main__':
 
     # Teste func 2
     print(get_synonym('Eu sou o primeiro texto de antonio pires, incluindo leis, resoluções, normas legais.'))
+
+    # Teste func 3
+    print(get_synonym_by_dict('Eu sou o primeiro texto de antonio pires, incluindo leis, resoluções, normas legais.'))
